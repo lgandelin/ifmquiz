6b7ec5cbeaee4c3ff4b72df8f4bdbd0a190e9520
Louis Gandelin <lgandelin@web-access.fr>
2018-05-04 15:24:56 +0200

    Started CSS integration

--- src/resources/views/back/includes/header.blade.php
+++ src/resources/views/back/includes/header.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.css" />
-    <link rel="stylesheet" href="/css/quiz.css" />
+    <link rel="stylesheet" href="/css/back.css" />
 </head>
 <body>
     <div class="page-wrapper">
--- src/resources/views/back/quiz/index.blade.php
+++ src/resources/views/back/quiz/index.blade.php
@@ -1,35 +1,73 @@
 @extends('ifmquiz::back.master')
 
 @section('main-content')
+    
+    <div class="header">
+        <div class="container">
+            <h1 class="title">Dashboard</h1>
+            <h2 class="subtitle">Questionnaires</h2>
+        </div>
+    </div>
 
-    <div class="container">
-        <h1 class="title">Dashboard</h1>
+    <div class="container dashboard-template">
+        <div class="list">
+            <div class="item add">
+                <div class="create-quiz">
+                    <span class="plus">+</span>
+                    <span class="create">Créer un<br/> questionnaire</span>
+                </div>
 
-        <a class="button is-primary" style="margin-bottom: 2rem;" href="{{ route('quiz_create') }}">Créer un questionnaire</a>
+                <div class="select-quiz-type">
+                    <a href="" class="back">Retour</a>
+                    <span class="select-type">Sélectionnez le type<br/> de questionnaire</span>
+                    
+                    <a class="button" href="{{ route('quiz_create') }}">Examen</a>
+                    <a class="button" href="{{ route('quiz_create') }}">Sondage</a>
+                </div>
+            </div>
 
-        <div class="tiles">
             @foreach ($quizs as $quiz)
-                <div class="tile box">
-                    <div>
-                        <h3 class="title">{{ $quiz->title }}</h3>
-                        <p>Taux de complétion : <span class="is-pulled-right">{{ 100*$quiz->completion }}%</span><br/>
-                            <progress class="progress is-warning" value="{{ 100*$quiz->completion }}" max="100"></progress>
-                        </p>
-                        <br/>
+                <div class="item">
+                    <span class="menu-icon"></span>
+                    <h3 class="title">{{ $quiz->title }}</h3>
+                    <span class="type">Type : Examen</span>
+                    <div class="progress-bar">Taux de complétion <span class="is-pulled-right">{{ 100*$quiz->completion }}%</span><br/>
+                        <progress class="progress" value="{{ 100*$quiz->completion }}" max="100"></progress>
+                    </div>
 
-                        <p>Note moyenne : <span class="is-pulled-right">{{ round($quiz->average, 1) }}/{{ $quiz->questions_number }}</span><br/>
-                            <progress class="progress is-warning" value="{{ $quiz->average/$quiz->questions_number }}" max="1"></progress>
-                        </p>
-                        <br/>
+                    <div class="progress-bar">Note moyenne <span class="is-pulled-right">{{ round($quiz->average, 1) }}/{{ $quiz->questions_number }}</span><br/>
+                        <progress class="progress" value="{{ ($quiz->questions_number > 0) ? ($quiz->average/$quiz->questions_number) : 0 }}" max="1"></progress>
+                    </div>
+                    <br/>
 
-                        <a class="button is-primary" href="{{ route('quiz_update', ['uuid' => $quiz->id]) }}">Editer</a>
-                        <a class="button is-warning" href="{{ route('quiz_results', ['uuid' => $quiz->id]) }}">Résultats</a>
-                        <a class="button is-link" href="{{ route('quiz_duplicate', ['uuid' => $quiz->id]) }}">Dupliquer</a>
-                        <a class="button is-danger" href="{{ route('quiz_delete', ['uuid' => $quiz->id]) }}">Supprimer</a>
+                    <a class="button" href="{{ route('quiz_update', ['uuid' => $quiz->id]) }}">Editer</a>
+                    <a class="button" href="{{ route('quiz_results', ['uuid' => $quiz->id]) }}">Résultats</a>
+
+                    <div class="menu">
+                        <a href="{{ route('quiz_duplicate', ['uuid' => $quiz->id]) }}">Dupliquer</a>
+                        <a href="{{ route('quiz_delete', ['uuid' => $quiz->id]) }}">Supprimer</a>    
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.add .create-quiz').click(function() {
+                $(this).parent().toggleClass('selecting');
+            });
+
+            $('.add .select-quiz-type .back').click(function(e) {
+                $(this).closest('.add').toggleClass('selecting');
+                e.preventDefault();
+            });
+
+            $('.item .menu-icon').click(function(e) {
+                $(this).closest('.item').find('.menu').toggle();
+            });
+        });
+    </script>
 
 @endsection
