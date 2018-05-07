56bbe210daf11043ba3a44850752426684a8cfa8
Louis Gandelin <lgandelin@web-access.fr>
2018-05-07 14:22:44 +0200

    Added integration for mailing and parameters template

--- src/resources/views/back/quiz/parameters.blade.php
+++ src/resources/views/back/quiz/parameters.blade.php
@@ -1,30 +1,45 @@
 @extends('ifmquiz::back.master')
 
 @section('main-content')
+    
+    <div class="header">
+        <div class="container">
+            <a class="button back-button" href="{{ route('quiz_update', ['uuid' => $quiz->id]) }}">Retour</a>
+            <h1 class="title">Paramètres</h1>
+            <h2 class="subtitle">{{ $quiz->title }}</h2>
+        </div>
+    </div>
 
     <div class="container">
-        <h1 class="title">Paramètres</h1>
-        <a class="button is-text" style="float:right" href="{{ route('quiz_update', ['uuid' => $quiz->id]) }}">Retour</a>
-        <h2>{{ $quiz->title }}</h2>
+        <div class="container page-template parameters-template">
+            <div class="page-content">
+                <form action="{{ route('quiz_parameters_handler', ['uuid' => $quiz->id]) }}" method="post">
 
-        <form action="{{ route('quiz_parameters_handler', ['uuid' => $quiz->id]) }}" method="post">
-            <div class="field">
-                <label class="label">Texte d'introduction</label>
-                <div class="control">
-                    <textarea class="textarea" placeholder="Texte d'introduction" name="intro_text" id="intro_text_editor">{!! $quiz->intro_text !!}</textarea>
-                </div>
+                    <div class="block">
+                        <div class="block-header">
+                            <h3 class="block-title">Texte d'introduction</h3>
+                        </div>
+                        
+                        <div class="block-content">
+                            <textarea class="textarea" placeholder="Texte d'introduction" name="intro_text" id="intro_text_editor">{!! $quiz->intro_text !!}</textarea>
+                        </div>
+                    </div>
+
+                    <div class="block">
+                        <div class="block-header">
+                            <h3 class="block-title">Texte de remerciement</h3>
+                        </div>
+
+                        <div class="block-content">
+                            <textarea class="textarea" placeholder="Texte de remerciement" name="outro_text" id="outro_text_editor">{!! $quiz->outro_text !!}</textarea>
+                        </div>
+
+                        {{ csrf_field() }}
+                        <input type="submit" class="button submit" value="Sauvegarder" />
+                    </div>
+                </form>
             </div>
-
-            <div class="field">
-                <label class="label">Texte de remerciement</label>
-                <div class="control">
-                    <textarea class="textarea" placeholder="Texte de remerciement" name="outro_text" id="outro_text_editor">{!! $quiz->outro_text !!}</textarea>
-                </div>
-            </div>
-
-            {{ csrf_field() }}
-            <input type="submit" class="button is-primary" value="Sauvegarder" />
-        </form>
+        </div>
     </div>
     <script src="https://cdn.ckeditor.com/ckeditor5/10.0.0/classic/ckeditor.js"></script>
     <script>
