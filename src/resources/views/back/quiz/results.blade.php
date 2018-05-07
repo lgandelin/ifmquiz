c8ff4974340cf2ae834e50e3dae1232756353533
Louis Gandelin <lgandelin@web-access.fr>
2018-05-07 15:37:44 +0200

    Added results integration

--- src/resources/views/back/quiz/results.blade.php
+++ src/resources/views/back/quiz/results.blade.php
@@ -1,17 +1,22 @@
 @extends('ifmquiz::back.master')
 
 @section('main-content')
+    
+    <div class="header">
+        <div class="container">
+            <a class="button back-button" href="{{ route('quiz_list') }}">Retour</a>
+            <h1 class="title">Résultats</h1>
+            <h2 class="subtitle">{{ $quiz->title }}</h2>
+        </div>
+    </div>
 
-    <div class="container">
-        <h1 class="title">Résultats</h1>
-        <a class="button is-text" style="float:right" href="{{ route('quiz_list') }}">Retour</a>
-        <h2>{{ $quiz->title }}</h2>
-
-        <div class="field" style="width:30%">
-            <input id="user-filter" type="text" class="input" placeholder="Recherche..." />
+    <div class="container page-template results-template">
+        <div class="search">
+            <span class="search-label">Recherche</span>
+            <input id="user-filter" type="text" class="search-input" placeholder="Nom / Prénom" />
         </div>
 
-        <table id="user-results" class="table is-bordered">
+        <table id="user-results" class="table">
             <thead>
                 <tr>
                     <th>Nom</th>
@@ -20,32 +25,32 @@
                     @foreach ($questions as $i => $question)
                         <th>Q{{ $i+1 }}</th>
                     @endforeach
-                    <th>Réponses</th>
                     <th>Résultats</th>
+                    <th>Réponses</th>
                 </tr>
             <thead>
             <tbody>
                 @foreach ($attempts as $attempt)
-                    <tr class="status-{{ $attempt->status }}">
+                    <tr>
                         <td>{{ $attempt->user->last_name }}</td>
                         <td>{{ $attempt->user->first_name }}</td>
                         <td>{{ $attempt->user->email }}</td>
                         @foreach ($attempt->answers as $answer)
-                            <td>{{ $answer }}</td>
+                            <td class="center no-border"><span @if ($answer > 0)style="border:1px solid orange"@endif>{{ $answer }}</span></td>
                         @endforeach
-                        <td><a href="{{ route('quiz_user_answers', ['quiz_id' => $quiz->id, 'attempt_id' => $attempt->id]) }}">Voir les réponses</a></td>
                         <td>{{ $attempt->result }}</td>
+                        <td class="center"><a href="{{ route('quiz_user_answers', ['quiz_id' => $quiz->id, 'attempt_id' => $attempt->id]) }}">Voir</a></td>
                     </tr>
                 @endforeach
             </tbody>
             <tfoot>
-                <tr class="is-selected">
-                    <td colspan="3">Moyenne</td>
+                <tr>
+                    <td colspan="3" class="right">Moyenne</td>
                     @foreach ($average_by_questions as $i => $question)
                         <td>{{ round($question, 1) }}</td>
                     @endforeach
-                    <td></td>
                     <td>{{ round($average_result, 1) }}/{{ sizeof($questions) }}</td>
+                    <td></td>
                 </tr>
             </tfoot>
         </table>
